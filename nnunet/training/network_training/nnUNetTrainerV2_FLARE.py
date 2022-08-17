79982a5c2137d4abc20176286276b13ef3c19e2e
ziyan huang <81061735+Ziyan-Huang@users.noreply.github.com>
2022-08-17 10:05:59 +0800

    Add files via upload

---
+++ nnunet/training/network_training/nnUNetTrainerV2_FLARE.py
@@ -0,0 +1,133 @@
+import torch
+from nnunet.network_architecture.generic_UNet import Generic_UNet
+from nnunet.network_architecture.initialization import InitWeights_He
+from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
+from nnunet.utilities.nd_softmax import softmax_helper
+from torch import nn
+import time
+
+class nnUNetTrainerV2_S6_D2_W16_FLARE(nnUNetTrainerV2):
+    def initialize_network(self):
+        self.conv_per_stage = 2
+        self.base_num_features = 16
+        self.max_num_features = 256
+        self.max_num_epochs = 1500
+        
+        # 取消验证，加速训练
+        self.num_val_batches_per_epoch = 1
+        self.save_best_checkpoint = False
+
+        
+        conv_op = nn.Conv3d
+        dropout_op = nn.Dropout3d
+        norm_op = nn.InstanceNorm3d
+
+        norm_op_kwargs = {'eps': 1e-5, 'affine': True}
+        dropout_op_kwargs = {'p': 0, 'inplace': True}
+        net_nonlin = nn.LeakyReLU
+        net_nonlin_kwargs = {'negative_slope': 1e-2, 'inplace': True}
+        
+        self.network = Generic_UNet(self.num_input_channels, self.base_num_features, self.num_classes,
+                                    len(self.net_num_pool_op_kernel_sizes),
+                                    self.conv_per_stage, 2, conv_op, norm_op, norm_op_kwargs, dropout_op, dropout_op_kwargs,
+                                    net_nonlin, net_nonlin_kwargs, True, False, lambda x: x, None, #InitWeights_He(1e-2), 取消这个初始化，提速 0.1s
+                                    self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True, True, self.max_num_features)
+
+        # # quantize
+        # start = time.time()
+        # self.network = torch.quantization.quantize_dynamic(
+        #                 self.network,  # the original model
+        #                 {nn.Conv3d},  # a set of layers to dynamically quantize
+        #                 dtype=torch.qint8)
+        # print('quantization time: ', time.time()-start)
+
+        start = time.time()
+        if torch.cuda.is_available():
+            self.network.cuda()
+        self.network.inference_apply_nonlin = softmax_helper
+        print('network to cuda time: ', time.time()-start)
+    
+    def setup_DA_params(self):
+        super().setup_DA_params()
+        self.data_aug_params["do_mirror"] = False
+        self.data_aug_params["do_elastic"] = True
+    
+        
+class nnUNetTrainerV2_S6_D3_W32_FLARE(nnUNetTrainerV2):
+    def initialize_network(self):
+        self.conv_per_stage = 3
+        self.base_num_features = 32
+        self.max_num_features = 512
+        self.max_num_epochs = 1000
+        
+        # 取消验证，加速训练
+        self.num_val_batches_per_epoch = 1
+        self.save_best_checkpoint = False
+        
+        conv_op = nn.Conv3d
+        dropout_op = nn.Dropout3d
+        norm_op = nn.InstanceNorm3d
+
+        norm_op_kwargs = {'eps': 1e-5, 'affine': True}
+        dropout_op_kwargs = {'p': 0, 'inplace': True}
+        net_nonlin = nn.LeakyReLU
+        net_nonlin_kwargs = {'negative_slope': 1e-2, 'inplace': True}
+
+        self.network = Generic_UNet(self.num_input_channels, self.base_num_features, self.num_classes,
+                                    len(self.net_num_pool_op_kernel_sizes),
+                                    self.conv_per_stage, 2, conv_op, norm_op, norm_op_kwargs, dropout_op, dropout_op_kwargs,
+                                    net_nonlin, net_nonlin_kwargs, True, False, lambda x: x, InitWeights_He(1e-2),
+                                    self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True, True, self.max_num_features)
+        if torch.cuda.is_available():
+            self.network.cuda()
+        self.network.inference_apply_nonlin = softmax_helper
+        
+    def setup_DA_params(self):
+        super().setup_DA_params()
+        self.data_aug_params["do_elastic"] = True
+
+
+class nnUNetTrainerV2_S5_D2_W16_FLARE(nnUNetTrainerV2):
+    def initialize_network(self):
+        self.conv_per_stage = 2
+        self.stage_num = 5
+        self.base_num_features = 16
+        self.max_num_features = 256
+        self.max_num_epochs = 1500
+        
+        # 取消验证，加速训练
+        self.num_val_batches_per_epoch = 1
+        self.save_best_checkpoint = False
+        
+
+        if len(self.net_conv_kernel_sizes) > self.stage_num:
+            self.net_conv_kernel_sizes = self.net_conv_kernel_sizes[:self.stage_num]
+            self.net_num_pool_op_kernel_sizes = self.net_num_pool_op_kernel_sizes[:self.stage_num-1]
+
+        if self.threeD:
+            conv_op = nn.Conv3d
+            dropout_op = nn.Dropout3d
+            norm_op = nn.InstanceNorm3d
+
+        else:
+            conv_op = nn.Conv2d
+            dropout_op = nn.Dropout2d
+            norm_op = nn.InstanceNorm2d
+
+        norm_op_kwargs = {'eps': 1e-5, 'affine': True}
+        dropout_op_kwargs = {'p': 0, 'inplace': True}
+        net_nonlin = nn.LeakyReLU
+        net_nonlin_kwargs = {'negative_slope': 1e-2, 'inplace': True}
+        self.network = Generic_UNet(self.num_input_channels, self.base_num_features, self.num_classes,
+                                    len(self.net_num_pool_op_kernel_sizes),
+                                    self.conv_per_stage, 2, conv_op, norm_op, norm_op_kwargs, dropout_op, dropout_op_kwargs,
+                                    net_nonlin, net_nonlin_kwargs, True, False, lambda x: x, None, #InitWeights_He(1e-2), 取消这个初始化，提速 0.1s
+                                    self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True, True, self.max_num_features)
+        if torch.cuda.is_available():
+            self.network.cuda()
+        self.network.inference_apply_nonlin = softmax_helper
+        
+    def setup_DA_params(self):
+        super().setup_DA_params()
+        self.data_aug_params["do_mirror"] = False
+        self.data_aug_params["do_elastic"] = True
