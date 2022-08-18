7473a19224251ac61c27087f79a4376f483b3f89
ziyan huang <81061735+Ziyan-Huang@users.noreply.github.com>
2022-08-18 11:40:32 +0800

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -3,6 +3,6 @@ export RESULTS_FOLDER="$PWD/RESULTS_FOLDER/"
 export nnUNet_raw_data_base="$PWD/nnUNet_raw_data_base/"
 export nnUNet_preprocessed="$PWD/nnUNet_preprocessed/"
 
-nnUNet_predict -i /workspace/inputs/  -o /workspace/outputs  -t 126  -p nnUNetPlansFLARE22Small   -m 3d_fullres \
- -tr nnUNetTrainerV2_S5_D2_W16_FLARE  -f all   --step_size 0.5 --mode fastest --disable_tta
+nnUNet_predict -i /workspace/inputs/  -o /workspace/outputs  -t 26  -p nnUNetPlansFLARE22Small   -m 3d_fullres \
+ -tr nnUNetTrainerV2_FLARE_Small  -f all  --mode fastest --disable_tta
 
